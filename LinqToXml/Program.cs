c32a23d3c2c65760907ccd2089521388cabed697
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-08-05 12:50:12 +0200

    Create new XML file.

--- LinqToXml/Program.cs
+++ LinqToXml/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LinqToXml
 {
@@ -10,7 +11,14 @@ namespace LinqToXml
     {
         static void Main(string[] args)
         {
-
+            // Create new XML file
+            XDocument doc = new XDocument(
+                    new XElement(
+                            "Modules",
+                            new XElement("Module", "Introduction to LINQ"),
+                            new XElement("Module", "LINQ and C#")
+                        )
+                );
 
             Console.ReadLine();
         }

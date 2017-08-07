3063fbf4b4a29cd5f9e1d9edf4a9b6ca4c76b4f1
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-08-07 16:21:23 +0200

    New features.

--- LinqToXml/Program.cs
+++ LinqToXml/Program.cs
@@ -11,6 +11,28 @@ namespace LinqToXml
     {
         static void Main(string[] args)
         {
+            CreateSimpleXml();
+            ReadSimpleXml();
+
+            Console.ReadLine();
+        }
+
+        private static void ReadSimpleXml()
+        {
+            XDocument doc = XDocument.Load("modules.xml");
+
+            XElement root = doc.Root;
+
+            var elements = root.Descendants();
+            foreach (var element in elements)
+            {
+                string value = element.Value;
+                Console.WriteLine(value);
+            }
+        }
+
+        public static void CreateSimpleXml()
+        {
             // Create new XML file
             XDocument doc = new XDocument(
                     new XElement(
@@ -21,8 +43,6 @@ namespace LinqToXml
                 );
 
             doc.Save(fileName: "modules.xml");
-
-            Console.ReadLine();
         }
     }
 }

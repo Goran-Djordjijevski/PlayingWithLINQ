383ac515cb79b09d45bd82152cdc82dbc8d389b5
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-30 21:13:26 +0100

    Added a new playground for the Linq.

---
+++ LinqQueries/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Filter Where operator
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+            };
+
+            var filteredResult = studentList.Where(s => s.Age > 12 && s.Age < 20);
+
+            Console.ReadLine();
+        }
+    }
+}

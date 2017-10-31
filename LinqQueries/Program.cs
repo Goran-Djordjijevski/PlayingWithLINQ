63d019a44121fe9f4271394881413a23c3ce35f3
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-31 20:53:46 +0100

    Sorting filters.

--- LinqQueries/Program.cs
+++ LinqQueries/Program.cs
@@ -32,7 +32,19 @@ namespace LinqQueries
 
             var stringResult = mixedList.OfType<string>();
 
-            // Filtering - OfType operator
+            // Sorting - OrderBy/OrderByDescending operator
+            IList<Student> studentListOrderBy = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 },
+                new Student() { StudentID = 6, StudentName = "Ram" , Age = 18 }
+            };
+
+            var studentsInAscOrder = studentListOrderBy.OrderBy(s => s.StudentName);
+            var studentsInDescOrder = studentListOrderBy.OrderByDescending(s => s.StudentName);
+            var studentsOrderThenBy = studentListOrderBy.OrderBy(s => s.StudentName).ThenBy(s => s.Age);
 
             Console.ReadLine();
         }

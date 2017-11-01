c219a8be65e3b474ea7b133cf6afc28aef168326
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-11-01 22:14:30 +0100

    Contains method syntax.

--- LinqQueries/Program.cs
+++ LinqQueries/Program.cs
@@ -136,6 +136,18 @@ namespace LinqQueries
             bool areAllStudentsTeenager = studentListAll.All(s => s.Age > 12 && s.Age < 20);
             bool isAnyStudentTeenager = studentListAll.Any(s => s.Age > 12 && s.Age < 20);
 
+            IList<int> intList = new List<int>() { 1, 2, 3, 4, 5 };
+            bool result = intList.Contains(15);
+
+            IList<Student> studentListContains = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+             };
+
+
             Console.ReadLine();
         }
     }

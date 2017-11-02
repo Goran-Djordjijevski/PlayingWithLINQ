50085c9348dad385ae8861df2b4a43d18a6ffa2d
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-11-02 20:14:32 +0100

    Equality - SequenceEqual method syntax.

--- LinqQueries/Program.cs
+++ LinqQueries/Program.cs
@@ -222,6 +222,12 @@ namespace LinqQueries
 
             string res19 = emptyListSingle.SingleOrDefault();
 
+            // Equality - SequenceEqual
+            IList<string> strList1 = new List<string>() { "One", "Two", "Three", "Four", "Three" };
+            IList<string> strList2 = new List<string>() { "One", "Two", "Three", "Four", "Three" };
+
+            bool isEqual = strList1.SequenceEqual(strList2);
+
             Console.ReadLine();
         }
     }
